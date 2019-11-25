d19fdeace95317b9f87eff56561000a56db6ca54
lkmaxwalton <58192962+lkmaxwalton@users.noreply.github.com>
2019-11-25 17:50:37 -0600

    Add files via upload

--- Model.java
+++ Model.java
@@ -1,42 +1,15 @@
 package application;
 
-public class Model implements ModelInterface {
-
-	private int numberOfCredits;
-
-	private String yearInSchool;
-
-	public Model(int numberOfCredits) {
-		setNumberOfCredits(numberOfCredits);
-	}
-
-	public void setNumberOfCredits(int newNumberOfCredits) {
-		numberOfCredits = newNumberOfCredits;
-		if (numberOfCredits >= 0 && numberOfCredits <= 30) {
-			yearInSchool = "Freshman";
-		} else if (numberOfCredits >= 31 && numberOfCredits <= 60) {
-			yearInSchool = "Sophomore";
-		} else if (numberOfCredits >= 61 && numberOfCredits <= 90) {
-			yearInSchool = "Junior";
-		} else if (numberOfCredits >= 91 && numberOfCredits <= 122) {
-			yearInSchool = "Senior";
-		}
-	}
-
-	@Override
+public class Model {
 	public void setIndependent(int newIndependent) {
-		setNumberOfCredits(newIndependent);
-
 	}
-
-	@Override
 	public int getIndependent() {
-		return numberOfCredits;
+		return 0;
 	}
-
-	@Override
+	
 	public String getDependent() {
-		return yearInSchool;
+		return null;
 	}
-
+	
+	
 }
